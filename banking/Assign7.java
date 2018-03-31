6cc04aedb139e174e5ea8b4b01ec87f4c17a18c1
Jamie Harnum <jaharnum@gmail.com>
2018-03-30 23:03:12 -0400

    Add initial code written by lab instructor

---
+++ banking/Assign7.java
@@ -0,0 +1,68 @@
+package banking;
+
+import java.util.Scanner; //best practice: only import the specific classes you actually will be using rather than java.util.*
+
+public class Assign7 {
+
+	public static Scanner in = new Scanner(System.in);
+
+	public static void displayMenu() {
+		
+		System.out.println("Enter your choice:");
+		System.out.println("a: Add new account");
+		System.out.println("d: Display account details");
+		System.out.println("p: Print all accounts");
+		System.out.println("u: Update account balance");
+		System.out.println("m: Month-end update");
+		System.out.println("q: Quit");
+		
+	}
+	
+	public static void main(String[] args) {
+		
+		char option = 'x';
+		
+		Bank bank = new Bank("Bank Name");
+		
+		do {
+			
+			displayMenu();
+			
+			option = in.next().toLowerCase().charAt(0); //method chaining - convert to lowercase, consider only first character
+			
+			switch(option) {
+			
+			case 'a':
+				bank.addAccount();
+				break;
+				
+			case 'd':
+				bank.displayAccount();
+				break;
+				
+			case 'p':
+				bank.printAccountDetails();
+				
+			case 'u':
+				bank.updateAccount();
+				break;
+				
+			case 'm':
+				bank.monthlyUpdate();
+				break;
+				
+			case 'q': 
+				break;
+				
+			default:
+				System.out.println("I'm sorry, I didn't understand your choice. Please try again");
+			}
+			
+		} while (option !='q');
+		
+		in.close();
+		System.out.println("Have a nice day!");
+
+	}
+
+}
